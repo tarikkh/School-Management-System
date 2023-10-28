54c692422a0bf6eb3c7727d6026711cdfb37f9fb
Tarik Khoumri <tarik.khmr@gmail.com>
2023-10-28 15:21:02 +0100

    databse query update

--- DataBase/db.sql
+++ DataBase/db.sql
@@ -1,2 +1,30 @@
-create database sms_db;
-use sms_db;
+CREATE DATABASE sms_db;
+USE sms_db;	
+
+CREATE TABLE admin(
+	id int primary key AUTO_INCREMENT,
+    username varchar(20) NOT NULL,
+    password varchar(500) NOT NULL,
+    fname varchar(25) NOT NULL,
+    lname varchar(25) NOT NULL
+);
+
+CREATE TABLE teachers(
+	id int primary key AUTO_INCREMENT,
+    username varchar(20) NOT NULL,
+    password varchar(500) NOT NULL,
+    fname varchar(25) NOT NULL,
+    lname varchar(25) NOT NULL
+);
+
+CREATE TABLE students(
+	id int primary key AUTO_INCREMENT,
+    username varchar(20) NOT NULL,
+    password varchar(500) NOT NULL,
+    fname varchar(25) NOT NULL,
+    lname varchar(25) NOT NULL
+);
+
+INSERT INTO admin(username, password, fname, lname) values("admin", "$2y$10$EtBHncOqq9kVYOn0exwPnemUqklS6Y/P21rJTntePHScx16zzxPaq", "first", "admin");
+INSERT INTO teachers(username, password, fname, lname) values("teacher", "$2y$10$EtBHncOqq9kVYOn0exwPnemUqklS6Y/P21rJTntePHScx16zzxPaq", "first", "teacher");
+INSERT INTO students(username, password, fname, lname) values("student", "$2y$10$EtBHncOqq9kVYOn0exwPnemUqklS6Y/P21rJTntePHScx16zzxPaq", "first", "student");
